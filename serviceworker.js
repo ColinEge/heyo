6d3853c09708adcb320c5dbe463818f355acbe23
Colin Egerton <github@colinegerton.co.uk>
2025-05-27 20:09:14 +0100

    feat: Remove screenshots and add caching

--- serviceworker.js
+++ serviceworker.js
@@ -1,16 +1,21 @@
-var staticCacheName = "pwa";
+var staticCacheName = "pwa-v1";
+
+var filesToCache = [
+  "/",
+  "/index.html",
+  "/noise.mp3",
+  "/images/image.png"
+];
 
 self.addEventListener("install", function (e) {
   e.waitUntil(
     caches.open(staticCacheName).then(function (cache) {
-      return cache.addAll(["/"]);
+      return cache.addAll(filesToCache);
     })
   );
 });
 
 self.addEventListener("fetch", function (event) {
-  console.log(event.request.url);
-
   event.respondWith(
     caches.match(event.request).then(function (response) {
       return response || fetch(event.request);
